a14e93abc1a9e1862f79775789afc15827655586
Moe-hacker <moe-hacker@outlook.com>
2025-10-18 11:02:06 +0800

    ci: switch to alpine v3.22

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@ if [ -f /etc/resolv.conf ]; then
     rm /etc/resolv.conf
 fi
 echo nameserver 1.1.1.1 >/etc/resolv.conf
-echo https://dl-cdn.alpinelinux.org/alpine/edge/testing >>/etc/apk/repositories
+echo https://dl-cdn.alpinelinux.org/alpine/3.22/testing >>/etc/apk/repositories
 
 apk update --no-cache
 apk add wget make clang git libseccomp-dev libseccomp-static libcap-static libcap-dev xz-dev libintl libbsd-static libsemanage-dev libselinux-utils libselinux-static xz-libs zlib zlib-static libselinux-dev linux-headers libssl3 libbsd libbsd-dev gettext-libs gettext-static gettext-dev gettext python3 build-base openssl-misc openssl-libs-static openssl zlib-dev xz-dev openssl-dev automake libtool bison flex gettext autoconf gettext sqlite sqlite-dev pcre-dev wget texinfo docbook-xsl libxslt docbook2x musl-dev gettext gettext-asprintf gettext-dbg gettext-dev gettext-doc gettext-envsubst gettext-lang gettext-libs gettext-static
